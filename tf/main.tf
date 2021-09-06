efca862b6b1005c6f1948d3b51439d44ea3f739e
aspatil919 <68045526+aspatil919@users.noreply.github.com>
2021-09-06 14:46:24 +0530

    Add files via upload

---
+++ tf/main.tf
@@ -0,0 +1,52 @@
+provider "azurerm" {
+    version = ">=2.0.0"
+    subscription_id = var.var_subscription_id
+    client_id = var.var_client_id
+    tenant_id = var.var_tenant_id
+    client_secret = var.var_client_secret
+    features{}
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name = "DEV_RG"
+        storage_account_name = "strvdev"
+        container_name = "dev"
+        key = "mainfile1.tfstate"
+    }
+}
+
+
+module "m_resource_group" {
+ source = "./TF/modules/Resourcegroup"
+ var_location = var.var_location
+ var_RG_name = var.var_RG_name
+}
+
+module "m_storage_account" {
+ source = "./TF/modules/Storageaccount"
+var_location = var.var_location
+var_storage_name = var.var_Storage_name
+var_account_tier = var.var_account_tier
+var_account_replication_type = var.var_account_replication_type  
+}
+
+module "m_storage_container" {
+source = "./TF/modules/StorageContainer"
+  var_storage_containername = var.var_storage_containername
+  container_access_type = "private"
+}
+
+module "m_data_factory"{
+var_data_factoryname = var.var_data_factoryname
+var_location = var.var_location
+#resource_group_name = azurerm_resource_group.RG.name
+}
+
+module  "m_data_lake" {
+var_data_lakename = var.var_data_lakename
+# resource_group_name =azurerm_resource_group.RG.name
+location = var.var_location
+
+}
+
